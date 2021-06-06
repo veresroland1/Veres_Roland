c735c6f7466f8770a31a65fe405f381720a7f25b
veresroland1 <veres.roland1@gmail.com>
2021-06-06 17:05:10 +0200

    ElsőAdd project files.

---
+++ Veres_Roland/WebApplication1/Controllers/MunkakController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WebApplication1.Controllers
+{
+    public class MunkakController : ApiController
+    {
+
+        public HttpResponseMessage Get()
+        {
+            string query = @" 
+                    select MunkaID,MunkaNeve from
+                    dbo.Munkak
+                    ";
+            DataTable table = new DataTable();
+            using (var con = new SqlConnection(ConfigurationManager.
+                ConnectionStrings["VRUzemAppDB"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+
+        }
+
+        public string Post(Munkak mk)
+
+        {
+            try
+            {
+                string query = @"
+                        insert into dbo.Munkak values
+                        (
+                        '" + mk.MunkaNeve + @"'
+
+                        )
+                       ";
+
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["VRUzemAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Added Successfully!";
+            }
+            catch (Exception)
+            {
+                return "Failed to Add!";
+            }
+        }
+
+
+
+
+        public string Put(Munkak mk)
+
+        {
+            try
+            {
+                string query = @"
+                        update dbo.Munkak set MunkaNeve=
+                        '" + mk.MunkaNeve + @"'
+                        where MunkaID=" + mk.MunkaID + @"
+                       ";
+
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["VRUzemAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Updated Successfully!";
+            }
+            catch (Exception)
+            {
+                return "Failed to Update!";
+            }
+        }
+
+
+
+
+        public string Delete(int id)
+
+        {
+            try
+            {
+                string query = @"
+                        delete from dbo.Munka 
+                        where MunkaID=" + id + @"
+                       ";
+
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["VRUzemAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Deleted Successfully!";
+            }
+            catch (Exception)
+            {
+                return "Failed to Delete!";
+            }
+        }
+
+    }
+}
